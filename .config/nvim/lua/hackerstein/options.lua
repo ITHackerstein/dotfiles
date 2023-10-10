603afa05e16ccd44d631c471619e34bdf2eeb4cd
ITHackerstein <carelladavide1@gmail.com>
2023-10-10 16:58:53 +0200

    nvim: re-read updated files

--- .config/nvim/lua/hackerstein/options.lua
+++ .config/nvim/lua/hackerstein/options.lua
@@ -35,3 +35,7 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 
 -- Autoread
 vim.opt.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+	pattern = { "*" },
+	command = "if mode() != 'c' | checktime | endif"
+})
