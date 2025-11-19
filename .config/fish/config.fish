fd0507443f9a4f743086d6c9a2c24ce0f54f2100
ITHackerstein <carelladavide1@gmail.com>
2025-11-19 17:28:06 +0100

    fish, kitty: new theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -71,27 +71,39 @@ for flatpakdir in ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bi
   end
 end
 
-# Lackluster
-set -gx fish_color_end 7a7a7a
-set -gx fish_color_error ffaa88
-set -gx fish_color_quote 708090
-set -gx fish_color_param aaaaaa
-set -gx fish_color_option aaaaaa
-set -gx fish_color_normal CCCCCC
-set -gx fish_color_escape 789978
-set -gx fish_color_comment 555555
-set -gx fish_color_command CCCCCC
-set -gx fish_color_keyword 7a7a7a
-set -gx fish_color_operator 7788aa
-set -gx fish_color_redirection ffaa88
-set -gx fish_color_autosuggestion 2a2a2a
-set -gx fish_color_selection --background=555555
-set -gx fish_color_search_match --background=555555
-set -gx fish_pager_color_prefix 999999
-set -gx fish_pager_color_progress 555555
-set -gx fish_pager_color_completion cccccc
-set -gx fish_pager_color_description 7a7a7a
-set -gx fish_pager_color_selected_background --background=555555
+# Miasma
+set -l background     222222
+set -l foreground     c2c2b0
+set -l selection      e4c47a
+set -l comment        666666
+set -l red            685742
+set -l orange         b36d43
+set -l yellow         c9a554
+set -l green          5f875f
+set -l purple         78824b
+set -l sky            d7c483
+set -l pink           bb7744
+
+set -g fish_color_normal         $foreground
+set -g fish_color_command        $sky
+set -g fish_color_keyword        $pink
+set -g fish_color_quote          $yellow
+set -g fish_color_redirection    $foreground
+set -g fish_color_end            $orange
+set -g fish_color_error          $red
+set -g fish_color_param          $purple
+set -g fish_color_comment        $comment
+set -g fish_color_selection      --background=$selection
+set -g fish_color_search_match   --background=$selection
+set -g fish_color_operator       $green
+set -g fish_color_escape         $pink
+set -g fish_color_autosuggestion $comment
+
+set -g fish_pager_color_progress            $comment
+set -g fish_pager_color_prefix              $sky
+set -g fish_pager_color_completion          $foreground
+set -g fish_pager_color_description         $comment
+set -g fish_pager_color_selected_background --background=$selection
 
 function fish_greeting
 	fastfetch -c examples/8.jsonc
