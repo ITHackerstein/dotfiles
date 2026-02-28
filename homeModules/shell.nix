2809eacdd012d1c63e0a0c369941f237e6da4397
Davide Carella <carelladavide1@gmail.com>
2026-02-28 11:39:02 +0100

    homeModules/shell.nix: Added aliases for dotfiles.

--- homeModules/shell.nix
+++ homeModules/shell.nix
@@ -4,5 +4,12 @@
         enable = true;
         enableCompletion = true;
         initExtra = lib.mkIf config.custom.microfetch.enable "microfetch";
+        shellAliases = {
+            "dotfiles-edit" = "pushd ~/.dotfiles && nvim && popd";
+            "dotfiles-sync" = "pushd ~/.dotfiles && git pull --rebase && git push && popd";
+            "dotfiles-switch" = "home-manager switch --flake ~/.dotfiles";
+            "dotfiles-system-switch" = "sudo nixos-rebuild switch --flake ~/.dotfiles && home-manager switch --flake ~/.dotfiles";
+            "dotfiles-update" = "pushd ~/.dotfiles && nix flake update && sudo nixos-rebuild switch --flake . && home-manager switch --flake . && git add flake.lock && git commit -m 'Updated system' && git push && popd";
+        };
     };
 }
