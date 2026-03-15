4ea5a5b1c1d72b4d7f15d5a6495247ccca3718ac
Davide Carella <carelladavide1@gmail.com>
2026-03-15 15:16:39 +0100

    homeModules/stylix.nix: Enabled dunst styling.

--- homeModules/stylix.nix
+++ homeModules/stylix.nix
@@ -56,6 +56,7 @@ in
                 nixvim.enable = false;
                 waybar.addCss = false;
                 rofi.enable = false;
+                dunst.enable = true;
             };
         };
 
